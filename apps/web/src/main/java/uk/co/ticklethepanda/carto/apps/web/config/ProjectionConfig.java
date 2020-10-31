33e3e17ddba9b6420523648a5fcaf9632b24c65c
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2020-10-31 10:27:27 +0000

    Use non-deprecated new instance method

--- apps/web/src/main/java/uk/co/ticklethepanda/carto/apps/web/config/ProjectionConfig.java
+++ apps/web/src/main/java/uk/co/ticklethepanda/carto/apps/web/config/ProjectionConfig.java
@@ -5,17 +5,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.co.ticklethepanda.carto.core.projection.Projector;
 
+import java.lang.reflect.InvocationTargetException;
+
 @Configuration
 public class ProjectionConfig {
 
     @Bean(name = "projector")
     public Projector projector(
             @Value("${location.history.heatmap.projector}") String projectorName
-    ) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+    ) throws ClassNotFoundException,
+            IllegalAccessException,
+            InstantiationException,
+            NoSuchMethodException,
+            InvocationTargetException {
 
         Class<?> projectorClass = Class.forName(projectorName);
 
-        Object projector = projectorClass.newInstance();
+        Object projector = projectorClass.getDeclaredConstructor().newInstance();
 
         if (!(projector instanceof Projector)) {
             throw new IllegalArgumentException("\"location.history.heatmap.projector\" must be a type of Projector");
