fac5d9355fbdc9a91f6d60e89d4ec218cc6dbb25
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2022-08-27 15:58:38 +0100

    Add infra

---
+++ infra/resources.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">=4.28"
+    }
+
+  }
+  cloud {
+    organization = "TickleThePanda"
+    workspaces {
+      name = "location-history"
+    }
+  }
+
+  required_version = ">=1.2.8"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "location_history" {
+  tags = {
+    Managed = "true"
+  }
+
+  tags_all = {
+    Managed = "true"
+  }
+
+  arn            = "arn:aws:s3:::location-history"
+  bucket         = "location-history"
+  hosted_zone_id = "Z21DNDUVLTQW6Q"
+}
+
+resource "aws_s3_bucket_public_access_block" "location_history" {
+  bucket = aws_s3_bucket.location_history.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
