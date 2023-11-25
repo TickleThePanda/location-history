903aecf8d2d37f792da1bb66ee5f3db60565a52b
Panda Rushwood | TickleThePanda <panda@ticklethepanda.dev>
2023-11-25 21:44:53 +0000

    Add convenience getters

--- core/runner/src/main/java/uk/co/ticklethepanda/carto/apps/gallery/GalleryImage.java
+++ core/runner/src/main/java/uk/co/ticklethepanda/carto/apps/gallery/GalleryImage.java
@@ -2,4 +2,14 @@ package uk.co.ticklethepanda.carto.apps.gallery;
 
 import java.io.InputStream;
 
-public record GalleryImage(GalleryImageDefinition combination, InputStream stream) {}
+public record GalleryImage(GalleryImageDefinition combination, InputStream stream) {
+
+    public String getImageName() {
+        return combination.config().getName();
+    }
+
+    public String getFilterName() {
+        return combination.name();
+    }
+
+}
