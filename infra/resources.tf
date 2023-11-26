b4e192b5513f07632736b9c6d5de6ba15da09c4a
Panda Rushwood | TickleThePanda <panda@ticklethepanda.dev>
2023-11-26 21:34:20 +0000

    Revert "Leave AWS buckets for now"
    This reverts commit 8ce776af5dea8dde79105aaf1b0bed670621129d.

--- infra/resources.tf
+++ infra/resources.tf
@@ -23,18 +23,3 @@ provider "aws" {
     }
   }
 }
-
-resource "aws_s3_bucket" "location_history" {
-  arn            = "arn:aws:s3:::location-history"
-  bucket         = "location-history"
-  hosted_zone_id = "Z21DNDUVLTQW6Q"
-}
-
-resource "aws_s3_bucket_public_access_block" "location_history" {
-  bucket = aws_s3_bucket.location_history.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
