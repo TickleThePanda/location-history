553714dd172b014752f6347669738f397c4fc81c
Panda Rushwood | TickleThePanda <panda@ticklethepanda.dev>
2023-11-26 21:24:46 +0000

    Remove AWS

--- infra/resources.tf
+++ infra/resources.tf
@@ -1,10 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">=4.28"
-    }
-  }
   cloud {
     organization = "TickleThePanda"
     workspaces {
